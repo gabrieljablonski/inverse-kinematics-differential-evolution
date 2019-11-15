9e47363db647019abf5ae5f0345668d3b0a95ac0
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-11-15 16:48:47 -0300

    Update 3d.go

--- src/vectors/3d.go
+++ src/vectors/3d.go
@@ -36,11 +36,31 @@ func RandomVector3D(constraint utils.Range1D) Vector3D {
 	return NewVector3D(x, y, z)
 }
 
-func (v Vector3D) Distance(otherVector Vector3D) float64 {
-	dX := v.X - otherVector.X
-	dY := v.Y - otherVector.Y
-	dZ := v.Z - otherVector.Z
-	return math.Sqrt(dX*dX + dY*dY + dZ*dZ)
+func Vector3DFromMatrix(array *arrays.Array2D) (Vector3D, error) {
+	if array.NRows() == 1 && array.NColumns() == 4 {
+		return vector3DFromRow(array), nil
+	} else if array.NColumns() == 1 && array.NRows() == 4 {
+		return vector3DFromColumn(array), nil
+	}
+	return Vector3D{}, fmt.Errorf("invalid matrix")
+}
+
+func vector3DFromRow(array *arrays.Array2D) Vector3D {
+	return Vector3D{
+		array.GetValue( 0, 0),
+		array.GetValue( 0, 1),
+		array.GetValue( 0, 2),
+		array.GetValue( 0, 3),
+	}
+}
+
+func vector3DFromColumn(array *arrays.Array2D) Vector3D {
+	return Vector3D{
+		array.GetValue( 0, 0),
+		array.GetValue( 1, 0),
+		array.GetValue( 2, 0),
+		array.GetValue( 3, 0),
+	}
 }
 
 func (v Vector3D) AsRow() *arrays.Array2D {
@@ -53,9 +73,13 @@ func (v Vector3D) AsColumn() *arrays.Array2D {
 
 func (v Vector3D) Transform(matrix *arrays.Array2D) Vector3D {
 	transformed := matrix.Multiply(v.AsColumn())
-	x := transformed.GetValue(0, 0)
-	y := transformed.GetValue(1, 0)
-	z := transformed.GetValue(2, 0)
-	w := transformed.GetValue(3, 0)
-	return Vector3D{x, y, z, w}
+	vector, _ := Vector3DFromMatrix(transformed)
+	return vector
+}
+
+func (v Vector3D) Distance(otherVector Vector3D) float64 {
+	dX := v.X - otherVector.X
+	dY := v.Y - otherVector.Y
+	dZ := v.Z - otherVector.Z
+	return math.Sqrt(dX*dX + dY*dY + dZ*dZ)
 }
