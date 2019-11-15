f10ff5811686f3d7e6a100ec49b367154223b1d9
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-11-15 12:14:43 -0300

    Update plot_link_generations.py

--- plot_link_generations.py
+++ plot_link_generations.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, TextBox
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.patheffects as patheffects
 
 
 def main(filename):
@@ -32,6 +33,16 @@ def main(filename):
     fitness_ax = fig.add_axes([0.2, .88, .13, .03], label='Fitness')
     fitness_text = TextBox(fitness_ax, '', str('0.00000'))
 
+    x, y, z = target
+    target_text = f"    Target: {x:+.5f} {y:+.5f} {z:+.5f}"
+    target_annotation = ax.annotate(target_text, (-.01, .09), color='white', size=10)
+    target_annotation.set_path_effects([patheffects.withStroke(linewidth=3,
+                                                               foreground='black')])
+    zero5f = f"{0:+.5f}"
+    xyz_annotation = ax.annotate(f"Manipulator: {zero5f} {zero5f} {zero5f}", (-.019, .08), color='white', size=10)
+    xyz_annotation.set_path_effects([patheffects.withStroke(linewidth=3,
+                                                            foreground='black')])
+
     ax.plot(*zip(target), 'rx', markersize=10)
     colors = 'black', 'red', 'yellow', 'blue'
     link_plots = [
@@ -44,12 +55,12 @@ def main(filename):
         generation = generations[int(gen_slider.val)-1]
         link0, *links1_4 = generation
         last = link0
+
         for i, link in enumerate(links1_4):
             x_data, y_data, z_data = zip(last, link)
             link_plots[i].set_xdata(x_data)
             link_plots[i].set_ydata(y_data)
             link_plots[i].set_3d_properties(z_data)
-            fig.canvas.draw_idle()
             last = link
 
         def fitness(point, target):
@@ -58,13 +69,21 @@ def main(filename):
             return ((x-tx)**2+(y-ty)**2+(z-tz)**2)**.5
         fitness_text.set_val(f'{fitness(last, target):.5f}')
 
-    gen_slider.on_changed(update_plot)
+        x, y, z = last
+        xyz = f"Manipulator: {x:+.5f} {y:+.5f} {z:+.5f}"
+        xyz_annotation.set_text(xyz)
 
+        fig.canvas.draw_idle()
+
+    gen_slider.on_changed(update_plot)
     plt.show()
 
 
 if __name__ == '__main__':
     import sys
 
-    file_name = sys.argv[1]
+    if len(sys.argv) == 1:
+        file_name = "example_output.txt"
+    else:
+        file_name = sys.argv[1]
     main(file_name)
