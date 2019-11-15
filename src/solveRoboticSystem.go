24b24124529476d402476ddf7355c1197a665922
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-11-15 12:53:12 -0300

    update parameters

--- src/solveRoboticSystem.go
+++ src/solveRoboticSystem.go
@@ -19,13 +19,13 @@ import (
 
 const (
 	NumberOfJoints  = 4
-	PopulationSize  = 10
+	PopulationSize  = 15
 	CrossoverRate   = 0.5
 	WeightingFactor = 0.5
 	MaxGenerations  = 2000
 	TargetFitness   = 0.000
 	StallPeriod     = 50     // in generations
-	StallFactor     = 0.001  // 0~1
+	StallFactor     = 0.1 // 0~1
 )
 
 // https://en.wikipedia.org/wiki/Ackley_function
